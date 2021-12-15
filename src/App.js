6a32cb1a499fb6a877e470c626f5b3b36457474f
장민우 <jmw900@naver.com>
2021-12-15 20:56:38 +0900

    Merge Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Test Merge
         </p>
         <h1>Test Commit</h1>
         <h1>Test Commit</h1>
