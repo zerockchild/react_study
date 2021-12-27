6cf4b45ecaaf31fd5311ee5f12a0932095ed4ee0
장민우 <jmw900@naver.com>
2021-12-27 23:06:27 +0900

    Practice Movie App 7.0-7.1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [toDo,setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setTodo("");
+    setToDos(currentArray => [toDo, ...toDos]);
+  }
+  console.log(toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test Merge
-        </p>
-        <h1>Test Commit</h1>
-        <h1>Test Commit</h1>
-        <h1>hi~!</h1>
-        <h1>Test Branch Commit</h1>
-        <h1>hanwolf commit test!!!</h1>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+      <input onChange={onChange} 
+      value={toDo} 
+      type="text" 
+      placeholder='Write your to do...' 
+      />
+      <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((element, idx) => <li key={idx}>{element}</li>)}
+      </ul>
     </div>
   );
 }
