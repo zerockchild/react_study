37f78fb317295bdedb97c1ddc0a8893c1e5cd529
chimco <72544949+nsw1055@users.noreply.github.com>
2021-12-28 09:51:18 +0900

    rollback

---
+++ src/ToDo.js
@@ -0,0 +1,29 @@
+import {useState} from "react";
+
+function ToDo() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => {
+        setToDo(event.target.value);
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDo("");
+        setToDos((currentArray) => [toDo, ...currentArray]);
+    };
+    console.log(toDos);
+    return (
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+                <button>Add To Do</button>
+            </form>
+            <hr/>
+            {toDos.map((item, index) => <li key={index}>{item.toUpperCase()}</li>)}
+        </div>
+    );}
+export default ToDo;
