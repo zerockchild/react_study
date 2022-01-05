3624ffb4934795ff143f8fb6927eb58a65671c70
dongho YUN <76724739+dhboys@users.noreply.github.com>
2022-01-05 20:13:49 +0900

    error commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import {useState, useEffect} from "react";
+import Movie from "./components/Movie.js";
+import Home from "./routes/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-      );
-      const json = await response.json();
-      setMovies(json.data.movies);
-      setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  },[])
-  console.dir(movies)
-  return (
-    <div>
-      {loading ? <h1>Loading...</h1> : <div>{movies.map(movie => <div key={movie.id}>
-        <img src={movie.medium_cover_image} />
-        <h2>{movie.title}</h2>
-        <p>{movie.summary}</p>
-          <ul>
-            {movie.genres.map(g => <li key={g}>{g}</li>)}
-          </ul>
-        </div>)}</div>}
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Routes>
+  </Router>
 }
 
 export default App;
